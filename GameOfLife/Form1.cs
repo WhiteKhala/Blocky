2fe68257fb874fa0e80f50a609817daf7d24a17f
Emanuel <Emanuel@EMANUEL-HP>
2016-08-30 23:48:52 -0400

    Add project files.

---
+++ GameOfLife/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Form1 : Form
+    {
+        bool[,] mSpace = new bool[25, 25];
+        Timer mCount = new Timer();
+        int mGenerations = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            mCount.Enabled = true;
+            mCount.Interval = 30;
+            mCount.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // NewGeneration
+
+            mGenerations++;
+
+            toolStripStatusLabelGen.Text = "Generations: " + mGenerations.ToString();
+
+            graphicsPanel1.Invalidate();
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void graphicsPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen mEpipen = new Pen(Color.Red, 1);
+            Brush mLiveCellBrush = new SolidBrush(Color.Blue);
+
+            //Gotta switch to floats
+            int mWidth = graphicsPanel1.ClientSize.Width / mSpace.GetLength(0);
+            int mHeight = graphicsPanel1.ClientSize.Height / mSpace.GetLength(1);
+
+
+            for (int x = 0; x < mSpace.GetLength(0); x++)
+            {
+                for (int y = 0; y < mSpace.GetLength(1); y++)
+                {
+                    Rectangle mRectangle = Rectangle.Empty;
+                    mRectangle.X = x * mWidth;
+                    mRectangle.Y =  y * mHeight;
+                    mRectangle.Width = mWidth;
+                    mRectangle.Height = mHeight;
+
+                    if (mSpace[x,y] == true)
+                    {
+                        e.Graphics.FillRectangle(mLiveCellBrush, mRectangle.X, mRectangle.Y, mRectangle.Width, mRectangle.Height);
+                    }
+
+                    e.Graphics.DrawRectangle(mEpipen, mRectangle.X, mRectangle.Y, mRectangle.Width, mRectangle.Height);
+                }
+            }
+
+            mEpipen.Dispose();
+            mLiveCellBrush.Dispose();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void graphicsPanel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Do math as floats
+                int width = graphicsPanel1.ClientSize.Width / mSpace.GetLength(0);
+                int height = graphicsPanel1.ClientSize.Height / mSpace.GetLength(1);
+
+                int x = e.X / width;
+                int y = e.Y / height;
+
+                mSpace[x, y] = !mSpace[x, y];
+
+                graphicsPanel1.Invalidate();
+            }
+        }
+
+
+    }
+}
